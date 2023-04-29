4efad3b0ac556f9e1042290a35c0ced5613a85ed
krupal <krupalmewada17@gmail.com>
2023-04-29 20:53:23 +0530

    welcome page

---
+++ app/src/main/java/com/example/decarpooling/WelcomePage.kt
@@ -0,0 +1,19 @@
+package com.example.decarpooling
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+    }
+//    val handler = Handler()
+//    val runnable = Runnable {
+//        val i = Intent(this@MainActivity, MenuPage::class.java)
+//        startActivity(i)
+//    }
+//    handler.postDelayed(runnable, 4000)
+
+
+}
--- app/src/main/java/com/example/decarpooling/signinactivity.kt
+++ app/src/main/java/com/example/decarpooling/signinactivity.kt
@@ -1,6 +1,8 @@
 package com.example.decarpooling
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class signinactivity : AppCompatActivity() {
@@ -8,5 +10,10 @@ class signinactivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val buttonclick1=findViewById<Button>(R.id.signinbtn)
+        buttonclick1.setOnClickListener{
+            val inten1= Intent(this,WelcomePage::class.java)
+            startActivity(inten1)
+        }
     }
 }
