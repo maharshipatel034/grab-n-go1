7609b2427dbab2255d4d7defd423c7b2d6e52036
krupal <krupalmewada17@gmail.com>
2023-04-30 22:22:29 +0530

    Main Page 1

--- app/src/main/java/com/example/decarpooling/WelcomePage.kt
+++ app/src/main/java/com/example/decarpooling/WelcomePage.kt
@@ -1,19 +1,18 @@
 package com.example.decarpooling
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_page)
-    }
-//    val handler = Handler()
-//    val runnable = Runnable {
-//        val i = Intent(this@MainActivity, MenuPage::class.java)
-//        startActivity(i)
-//    }
-//    handler.postDelayed(runnable, 4000)
-
+        val buttonclic=findViewById<Button>(R.id.openProfileInfoButton)
+        buttonclic.setOnClickListener {
+            val inte = Intent(this, MainPage::class.java)
+            startActivity(inte)
+        }}
 
 }
