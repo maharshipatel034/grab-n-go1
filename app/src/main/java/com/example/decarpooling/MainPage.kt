b8f6dd114ec1bcc9137d53d699edb609960d629f
krupal <krupalmewada17@gmail.com>
2023-05-04 21:38:01 +0530

    map included

--- app/src/main/java/com/example/decarpooling/MainPage.kt
+++ app/src/main/java/com/example/decarpooling/MainPage.kt
@@ -1,11 +1,19 @@
 package com.example.decarpooling
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+        val buttoncli=findViewById<ToggleButton>(R.id.searchCreateButton1)
+        buttoncli.setOnClickListener{
+            val inte1= Intent(this,MapsActivity::class.java)
+            startActivity(inte1)
+        }
+
     }
 }
